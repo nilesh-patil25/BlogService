dd194f6d777da67ec6b2f504f568c2d404200d28
nilesh-patil25 <nilesh.patil74136@gmail.com>
2023-12-15 14:18:58 +0530

    update

--- BlogService.Core/Services/Interfaces/IBlogPostService.cs
+++ BlogService.Core/Services/Interfaces/IBlogPostService.cs
@@ -6,8 +6,10 @@ namespace BlogService.Core.Services.Interfaces
 {
     public interface IBlogPostService
     {
-        Task<BlogPost> CreatePostAsync(BlogPostDTO blogPost);
+        Task<BlogPost> CreatePostAsync(BlogPostDTO blogPost); 
+
         Task<BlogPost> GetPostByIdAsync(int id);
+
         Task<IEnumerable<BlogPost>> GetAllBlogsAsync(Expression<Func<BlogPost, bool>>? filter = null, string? includeProperties = null);
     }
 }
